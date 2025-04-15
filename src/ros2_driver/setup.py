c21c400ca776b121ecbfc42ec997ef5462b5cddc
xrz <1477382223@qq.com>
2025-04-15 15:40:12 +0800

    V0

--- src/ros2_driver/setup.py
+++ src/ros2_driver/setup.py
@@ -9,22 +9,21 @@ setup(
     version='0.0.1',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Your Name',
+    maintainer='your-name',
     maintainer_email='your-email@example.com',
-    description='ROS2 driver package for robot control',
-    license='TODO',
+    description='ROS2 driver package',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'driver_node = ros2_driver.driver_node:main',
         ],
     },
-) 
+)
